f9b46b4620258ae877f4a0e9ae3ee9a76751bbe8
xgbnl <745502274@qq.com>
2024-06-20 17:33:47 +0800

    test

--- src/test/java/hema/container/ContainerTests.java
+++ src/test/java/hema/container/ContainerTests.java
@@ -1,6 +1,8 @@
 package hema.container;
 
 import hema.web.inflector.InflectorConfiguration;
+import org.json.JSONObject;
+import org.json.JSONWriter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,26 +20,34 @@ public class ContainerTests {
     @Autowired
     private ApplicationContext context;
 
-    private static Map<String, Object> data;
+    private static final Map<String, Object> data = new HashMap<>();
 
     @Test
     public void testFactoryNotNull() {
         assertNotNull(context.getBean(Factory.class));
     }
 
-    @Test
-    public void testRegularBeanInstantiation() {
-        Map<String, Object> data = new HashMap<>();
+    @BeforeAll
+    public static void before() {
+        Map<String,Object> address = new HashMap<>();
+        address.put("id",1);
+        address.put("user_id",1);
+        address.put("city","Chengdu");
+        address.put("address","Jinjiang");
+
         data.put("id", "1");
         data.put("name", "tom");
         data.put("email", "tom@hotmail.com");
-        data.put("address", "{\"id\":1,\"user_id\":1,\"city\":\"Chengdu\",\"address\":\"Shizhishan\"}");
+        data.put("address", new JSONObject(address).toString());
         data.put("status", "ENABLED");
+    }
 
+    @Test
+    public void testRegularBeanInstantiation() {
         Factory factory = context.getBean(Factory.class);
         User user = factory.make(User.class, data);
         assertNotNull(user);
-        System.out.println(user);
+
     }
 
 }
