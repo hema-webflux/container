bcc12f38def38ee226fa899b464fbb89b1107b9b
xgbnl <745502274@qq.com>
2024-07-01 17:25:57 +0800

    feat: add new annotation Entity

--- src/main/java/hema/container/annotation/Entity.java
+++ src/main/java/hema/container/annotation/Entity.java
@@ -1,4 +1,10 @@
 package hema.container.annotation;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface Entity {
 }
--- src/test/java/hema/container/Address.java
+++ src/test/java/hema/container/Address.java
@@ -1,4 +1,7 @@
 package hema.container;
 
+import hema.container.annotation.Entity;
+
+@Entity
 record Address(int id, int userId, String city, String address) {
 }
